87774ac620ec6032e1b8428634f04b24c489c4b9
shizu <shizuya-h@docomo.ne.jp>
2020-02-12 14:22:22 +0000

    3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true,
+                   length: { minmum: 1, maximum: 15 }
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    
+  has_secure_password  
+  validates :password, presence: true,
+                       lenght: 8..32,
+                       format: { with: /\A[a-zA-Z0-9]+\z/ }
   
-  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  get 'topics/new'
   get 'sessions/new'
   
   root 'pages#index'
   get 'pages/help'
   
-  resources :users
-  
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
   delete '/login', to: 'sessions#destory'
+  
+  resources :users
+  resources :topics
 end
